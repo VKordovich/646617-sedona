3df78929808aa3546bf283ba9d91b8b7751e31ce
VKordovich <VKordovich@gmail.com>
2018-09-04 16:33:00 +0500

    анимация модального окна
    добавлена анимация с помощью  js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 var button = document.querySelector(".modal-button");
-var window = document.querySelector(".modal");
+var modal = document.querySelector(".modal");
+var dateingo = document.querySelector("#dateingo");
+var dateoutgo = document.querySelector("#dateoutgo");
+var people = document.querySelector("#people");
+var kids = document.querySelector("#kids");
+var search = document.querySelector(".modal button");
 
-button.addEventListener("click", function(evt) {
+button.addEventListener("click", function (evt) {
   evt.preventDefault();
   console.log("Клик по кнопке");
-  window.classList.toggle("modal-vision");
+  modal.classList.toggle("modal-vision");
+  dateingo.focus();
+});
+
+search.addEventListener("submit", function (evt) {
+  if (!dateingo && !dateoutgo) {
+    evt.preventDefault();
+    console.log("Форма не заполнена");
+  } else if (!people && !kids) {
+    evt.preventDefault();
+    console.log("Форма не заполнена");
+  }
 });
